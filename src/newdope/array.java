0ef691074b0fbf06e5490c04214a58712d8680c8
Akashchandel27 <akashchandel27@gmail.com>
2022-10-03 16:50:58 +0530

    new update

--- src/newdope/array.java
+++ src/newdope/array.java
@@ -5,7 +5,9 @@ public class array {
 	public static void main(String[] args) {
 		
 		int sum=0;
-		int[] a = new int[] { 10, 20, 5, 70, 4 };
+
+
+		int[] a = new int[] { 10, 20, 5, 70, 4 }; // array
 		for (int a1:a) {
 			//System.out.println(a1);
 			sum=sum+a1;
